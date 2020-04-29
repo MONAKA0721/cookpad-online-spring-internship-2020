6dbc75cde4d675fdfa781827c27025f47f14c685
Yuya Sumie <monamonacan0926@gmail.com>
2020-04-29 08:13:23 +0000

    try

--- main/app/services/recipe_service.rb
+++ main/app/services/recipe_service.rb
@@ -15,7 +15,6 @@ class RecipeService < Main::Services::V1::Recipe::Service
     page = request.page unless request.page.zero?
     per_page = request.per_page unless request.per_page.zero?
 
-    # TODO: Avoid to N+1 query, Use index
     recipes = Recipe.
       order(created_at: :desc).
       page(page).
